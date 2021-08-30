1cd0f07f456e99a2827764f98edafa46f0d31d99
Mauricio de Souza <55670845+xioumau@users.noreply.github.com>
2021-08-29 22:00:56 -0300

    interpolação

--- Program.cs
+++ Program.cs
@@ -120,7 +120,7 @@ namespace DIO.Series
 			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
 			foreach (int i in Enum.GetValues(typeof(Genero)))
 			{
-				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+				Console.WriteLine($"{i}-{(Enum.GetName(typeof(Genero), i))}");
 			}
 			Console.Write("Digite o gênero entre as opções acima: ");
 			int entradaGenero = int.Parse(Console.ReadLine());
