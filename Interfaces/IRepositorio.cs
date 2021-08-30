f488c816ec999f07cc105a2c7daa2f2ad26cddf7
Mauricio de Souza <55670845+xioumau@users.noreply.github.com>
2021-08-30 08:46:14 -0300

    Interface para classe FilmeRepositorio

--- Interfaces/IRepositorio.cs
+++ Interfaces/IRepositorio.cs
@@ -11,4 +11,14 @@ namespace DIO.Series.Interfaces
         void Atualiza(int id, T entidade);
         int ProximoId();
     }
+
+    public interface IRepositorioFilme<T>
+    {
+        List<T> ListaFilme();
+        T RetornaPorIdFilme(int id);        
+        void InsereFilme(T entidade);        
+        void ExcluiFilme(int id);        
+        void AtualizaFilme(int id, T entidade);
+        int ProximoIdFilme();
+    }
 }
